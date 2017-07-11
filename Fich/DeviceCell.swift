0c27cff6c7aaae914c0c4139e2f3b438b902e7ab
phapli <phapli1991@gmail.com>
2017-07-12 01:56:45 +0700

    Scan BLE

---
+++ Fich/DeviceCell.swift
@@ -0,0 +1,27 @@
+//
+//  DeviceCell.swift
+//  Fich
+//
+//  Created by Tran Tien Tin on 7/12/17.
+//  Copyright © 2017 fichteam. All rights reserved.
+//
+
+import UIKit
+
+class DeviceCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func onPair(_ sender: UIButton) {
+    }
+}
